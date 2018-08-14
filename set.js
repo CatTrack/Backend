1908803d884918f8395cdb9df10054a0bd1e6aeb
Benji <benji@benjicraft.info>
2018-08-14 16:04:19 +0100

    Fixed timestamps

--- set.js
+++ set.js
@@ -5,43 +5,17 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-function getDateTime() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
-
-}
-
 var db = admin.firestore();
 
-var userId = "0";
+var userId = "1";
 var catId = "0";
 var location = "Inside";
 var locId = "Bowl";
-var cDate = getDateTime();
+var cDate = new Date();
 
 var docRef = db.collection('users').doc(userId).collection('Cats').doc(catId);
 
 var dataStuff = docRef.set({
-  "Identifier": "John",
   "Location": {
       "General Location": location,
       "Specific Location": [
@@ -53,4 +27,4 @@ var dataStuff = docRef.set({
           } 
       ]
   }
-});
+}, {merge: true });
