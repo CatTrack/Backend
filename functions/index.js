caf66734432c4761fa3e3fd303693d2b63c9a331
Benji <benji@benjicraft.info>
2018-08-15 12:27:48 +0100

    Converted to JSON object

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
+// Required libraries
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+var serviceAccount = require("./servicekey.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.setLoc = functions.https.onRequest((req, res) => {
+            const q = req.query;
+            var reqres = {
+                "userId": q.userId,
+                "catId": q.catId,
+                "location": q.location,
+                "locId": q.locId
+            };
+
+            console.log(reqres);
+
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount)
+            });
+
+            var db = admin.firestore();
+
+            var cDate = new Date();
+
+            var docRef = db.collection('users').doc(reqres['userId']).collection('Cats').doc(reqres['catId']);
+
+            var dataStuff = docRef.set({
+                "Location": {
+                    "General Location": reqres['location'],
+                    "Specific Location": [{
+                        "Specific Location": {
+                            "Location Identifier": reqres['locId'],
+                            "Timestamps": [cDate]
+                        }
+                    }]
+                }
+            }, {
+                merge: true
+            });
+            res.status(200).send("Success");
+            return original;
+        });
