57bd40e11ec9a93ca81f9999c849e8ba67338913
Benji <benji@benjicraft.info>
2018-08-16 13:49:54 +0100

    Rearanged and made changes

--- functions/index.js
+++ functions/index.js
@@ -2,41 +2,71 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 var serviceAccount = require("./servicekey.json");
+var cDate = new Date();
 
 exports.setLoc = functions.https.onRequest((req, res) => {
-            const q = req.query;
-            var reqres = {
-                "userId": q.userId,
-                "catId": q.catId,
-                "location": q.location,
-                "locId": q.locId
-            };
-            
-            var reqstr = JSON.stringify(reqres);
+    var q = req.query;
+    var reqres = {
+        "userId": q.userId,
+        "catId": q.catId,
+        "location": q.location,
+        "locId": q.locId
+    };
 
-            admin.initializeApp({
-                credential: admin.credential.cert(serviceAccount)
-            });
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
 
-            var db = admin.firestore();
+    var db = admin.firestore();
 
-            var cDate = new Date();
+    var docRef = db.collection('users').doc(reqres.userId).collection('Cats').doc(reqres.catId);
 
-            var docRef = db.collection('users').doc(reqres['userId']).collection('Cats').doc(reqres['catId']);
-
-            var dataStuff = docRef.set({
-                "Location": {
-                    "General Location": reqres['location'],
-                    "Specific Location": [{
-                        "Specific Location": {
-                            "Location Identifier": reqres['locId'],
-                            "Timestamps": [cDate]
-                        }
-                    }]
+    var dataStuff = docRef.set({
+        "Location": {
+            "General Location": reqres.location,
+            "Specific Location": [{
+                "Specific Location": {
+                    "Location Identifier": reqres.locId,
+                    "Timestamps": [cDate]
                 }
-            }, {
-                merge: true
-            });
-            res.status(200).send("Received: \n "+ JSON.stringify(reqres) );
-            return "200";
-        });
+            }]
+        }
+    }, {
+        merge: true
+    });
+    res.status(200).send("Received: \n " + JSON.stringify(reqres));
+    return "200";
+});
+
+exports.newCat = functions.https.onRequest((req, res) => {
+    var q = req.query;
+    var reqres = {
+        "userId": q.userId,
+        "catId": q.catId,
+        "location": q.location,
+        "locId": q.locId
+    };
+    var dataStuff = {
+        "Identifier": q.catId,
+        "Location": {
+            "General Location": reqres.location,
+            "Specific Location": [{
+                "Specific Location": {
+                    "Location Identifier": reqres.locId,
+                    "Timestamps": [cDate]
+                }
+            }]
+        }
+    };
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    var db = admin.firestore();
+
+    var addDoc = db.collection('users').doc(reqres.userId).collection('Cats').add(dataStuff).then(ref => {
+        res.status(200).send('Added document with ID: '+ ref.id);
+    });
+    return "200";
+})
