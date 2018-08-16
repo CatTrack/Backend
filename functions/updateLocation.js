a824de4912650bb3d7c1238163782feaada2289e
Benji <benji@benjicraft.info>
2018-08-16 13:50:06 +0100

    Removed redundant file

--- functions/updateLocation.js
+++
@@ -1,8 +0,0 @@
-// Required libraries
-const functions = require('firebase-functions');
-const admin = require("firebase-admin");
-var serviceAccount = require("./servicekey.json");
-
-exports.setLoc = functions.https.onRequest((req, res) => {
-    console.log("Fire")
-});
