862c7c592cab4e8bcb546edc8917b11ad4a9c337
Ben Emmons <ben@emmons.biz>
2018-08-20 10:07:54 +0100

    Fixed weird github issues in 'Moved to correct directory.

--- functions/getCats.js
+++ functions/getCats.js
@@ -1,60 +1,13 @@
 const request = require("request");
 
-function debugInfo(error, response){
-    if (response.statusCode != 200){
-        console.log({"statusCode:": (response && response.statusCode)});
-    }
-    if (error != null){
-        console.log({"error:": error});
-    }
-}
-
-
-function usernameToID(targetUsername, callback){
-    let endpoint = "https://firestore.googleapis.com/v1beta1/projects/te-cattrack/databases/(default)/documents/users/";
-    request(endpoint, function (error, response, body) {
-        debugInfo(error, response);
-
-        body = JSON.parse(body);
-        body.documents.forEach(user => {
-            let username = user.fields.Username.stringValue;
-            if (username == targetUsername) {
-                let userID = user.name.split("projects/te-cattrack/databases/(default)/documents/users/")[1];
-                callback(null, userID);
-            }
-
-        });
-
-    });
-}
-
-
-function getCats(userID, callback){
+module.exports.getCats = function(userID, callback){
     let endpoint = "https://firestore.googleapis.com/v1beta1/projects/te-cattrack/databases/(default)/documents/users/";
     request(endpoint + userID + "/Cats", function (error, response, body) {
-        debugInfo(error, response)
-
         body = JSON.parse(body)
         var cats = [];
         body.documents.forEach(cat => {
             cats.push(cat.fields.Identifier.stringValue);
         });
         callback(null, cats);
-});
-}
-
-
-function main(username){
-  usernameToID(username, function(err, userID){
-    if (err != null){
-        console.log(err);
-    }
-
-    getCats(userID, function(err, cats){
-        console.log(username + "'s ID is: " + userID);
-        console.log(username + "'s cats are: " + cats);
     });
-  });
 }
-
-main("Ben");
